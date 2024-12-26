954873af49210b925f26b20589021c6bdf01269f
bapakfadil <sipadil28@hotmail.com>
2024-12-26 10:07:43 +0700

    Created API : Upload media image

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const isBase64 = require("is-base64");
+const base64Image = require("base64-img");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { Media } = require("../models");
+
+router.get("/", function (req, res) {
+  res.send("Service Media: Media route is working");
+});
+
+router.post("/", function (req, res) {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res
+      .status(400)
+      .json({ status: "Error", message: "Invalid Base64 Image" });
+  }
+
+  base64Image.img(
+    image,
+    "./public/images",
+    Date.now(),
+    async (err, filepath) => {
+      if (err) {
+        return res.status(400).json({ status: "Error", message: err.message });
+      }
+
+      const filename = filepath.split("\\").pop().split("/").pop();
+
+      const media = await Media.create({ image: `image/${filename}` });
+
+      return res.json({
+        status: "Success",
+        data: {
+          id: media.id,
+          image: `${req.get("host")}/images/${filename}`,
+        },
+      });
+    }
+  );
 });
 
 module.exports = router;
