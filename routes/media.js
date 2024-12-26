ca787b27709e84ae7e777204c8ae5c684d17ab8e
bapakfadil <sipadil28@hotmail.com>
2024-12-26 10:19:13 +0700

    Created API : Get all image

--- routes/media.js
+++ routes/media.js
@@ -5,8 +5,20 @@ const base64Image = require("base64-img");
 
 const { Media } = require("../models");
 
-router.get("/", function (req, res) {
-  res.send("Service Media: Media route is working");
+router.get("/", async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ["id", "image"],
+  });
+
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get("host")}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status: "success",
+    data: mappedMedia,
+  });
 });
 
 router.post("/", function (req, res) {
@@ -29,7 +41,7 @@ router.post("/", function (req, res) {
 
       const filename = filepath.split("\\").pop().split("/").pop();
 
-      const media = await Media.create({ image: `image/${filename}` });
+      const media = await Media.create({ image: `images/${filename}` });
 
       return res.json({
         status: "Success",
